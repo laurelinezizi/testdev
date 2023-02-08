bb14fe110e8d41e614de01fb7642c594e6409280
Laureline Zizi (LZZ) <lzz@odoo.com>
2023-02-08 10:24:28 +0100

    estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Estate',
+    'application': True,
+    'version': '1',
+    'category': 'Sales/CRM',
+    'sequence': 15,
+    'summary': 'Track all your building',
+    'description': "This module contains all the common features of Building Management.",
+    'depends': [
+        'base',
+        'crm',
+    ],
+    'data': [
+        'views/realestate.xml',
+}
+
---
+++ estate/models/realestate.py
@@ -0,0 +1,4 @@
+from odoo import models
+
+class RealEstate(models.Model):
+    _name = "real.estate"
