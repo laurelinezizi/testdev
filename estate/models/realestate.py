dcc73a50f2dc9f5fd27ba6414925bccf289ac384
Laureline Zizi (LZZ) <lzz@odoo.com>
2023-02-08 10:57:31 +0100

    estate

--- estate/models/realestate.py
+++ estate/models/realestate.py
@@ -1,4 +1,8 @@
-from odoo import models
+from odoo import fields, models
 
 class RealEstate(models.Model):
     _name = "real.estate"
+    _description = "Real Estate"
+
+    name = fields.Char()
+
