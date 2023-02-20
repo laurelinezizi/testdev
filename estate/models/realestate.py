8562930a4871c9268252cace25bdf94b3c8c0c8f
Laureline Zizi (LZZ) <lzz@odoo.com>
2023-02-20 11:03:36 +0100

    estate

--- estate/models/realestate.py
+++ estate/models/realestate.py
@@ -4,7 +4,7 @@ class RealEstate(models.Model):
     _name = "real.estate"
     _description = "Real Estate"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, index=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
