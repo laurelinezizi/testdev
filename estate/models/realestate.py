5c25adebbadb4e1ac42f3f1a51690b050258a9de
Laureline Zizi (LZZ) <lzz@odoo.com>
2023-02-20 10:39:11 +0100

    estate

--- estate/models/realestate.py
+++ estate/models/realestate.py
@@ -4,11 +4,11 @@ class RealEstate(models.Model):
     _name = "real.estate"
     _description = "Real Estate"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()
